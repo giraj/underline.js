2509879f69a3e7e8a837b39db5c5f7df801ed34a
Jarl Gunnar T. Flaten <jarl.flaten@gmail.com>
2013-10-15 20:15:42 +0200

    add changes from master, improve design, now a good usage example

--- underline.js
+++ underline.js
@@ -1,4 +1,6 @@
-// use data-something to make css for g, y, p, q etc
+// keeps track of underline instances for class naming
+var Underline_id = 0;
+
 function Underline(identifier, options) {
     'use strict';
     return this.init(identifier, options);
@@ -9,6 +11,7 @@ if (window.module !== undefined) {
 }
 
 (function (window, document) {
+    'use strict';
 
     // stolen from daviferreira's medium clone :)
     function extend(b, a) {
@@ -17,7 +20,7 @@ if (window.module !== undefined) {
             return a;
         }
         for (prop in a) {
-            if (a.hasOwnPropery(prop) && b.hasOwnProperty(prop) === false) {
+            if (a.hasOwnProperty(prop) && b.hasOwnProperty(prop) === false) {
                 b[prop] = a[prop];
             }
         }
@@ -26,7 +29,7 @@ if (window.module !== undefined) {
 
     // checks if element a is in array b
     function aInb(a, b) {
-        for (e in b) {
+        for (var e in b) {
             if (b[e] === a) {
                 return true;
             }
@@ -54,19 +57,23 @@ if (window.module !== undefined) {
 
     Underline.prototype = {
         defaults: {
-            margin: 5,
-            padding: 5,
+            margin: 0,
+            width: 1,
+            style: 'solid',
             color: 'black',
             noUnderline: ['g', 'j', 'y', 'p', 'q']
         },
 
         init: function (identifier, options) {
+            console.log(this.defaults.hasOwnProperty);
             this.elements = typeof identifier === 'string' ? document.querySelectorAll(identifier) : identifier;
             this.options = extend(options, this.defaults);
-            this.underline();
+            this.id = Underline_id++;
+            this.format()
+                .underline();
         },
 
-        underline: function () {
+        format: function () {
             var i,
                 s,
                 result,
@@ -79,16 +86,16 @@ if (window.module !== undefined) {
                 e.innerHTML = '';
 
                 result = '';
-                child = initChild('span', 'underline-text');
+                child = initChild('span', 'underline-text' + this.id);
                 for (s in string) {
                     if (aInb(string[s], this.options.noUnderline)) {
                         e.appendChild( appendTextNode(child, result) );
 
-                        child = initChild('span', 'nounderline-text');
+                        child = initChild('span', 'nounderline-text' + this.id);
                         e.appendChild( appendTextNode(child, string[s]) );
 
                         result = '';
-                        child = initChild('span', 'underline-text');
+                        child = initChild('span', 'underline-text' + this.id);
                     }
                     else {
                         result += string[s];
@@ -97,6 +104,29 @@ if (window.module !== undefined) {
                 e.appendChild( appendTextNode(child, result) );
             }
             return this;
+        },
+
+        underline: function() {
+            var head = document.getElementsByTagName('head')[0],
+                s = document.createElement('style');
+            // holy shit this is bad
+            var css = ".underline-text" + this.id + ":after {";
+                css += "content: '';";
+                css += "position: absolute;";
+                css += "border-bottom: " + this.options.width + 'px ' + this.options.style + ' ' + this.options.color + ';';
+                css += "height: 1.15em; /* insane default.. */";
+                css += "left: 0;";
+                css += "right: 0;";
+                css += "margin-top: " + this.options.margin + 'px;';
+                css += '}';
+                css += ".underline-text" + this.id + " { position:relative; } ";
+            s.setAttribute('type', 'text/css');
+            if (s.styleSheet) {   // IE
+                s.styleSheet.cssText = css;
+            } else {                // the world
+                s.appendChild(document.createTextNode(css));
+            }
+            head.appendChild(s);
         }
     }
 })(window, document);
